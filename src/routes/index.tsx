43c801e529ec93bd5b4d59376d6b7076eb8a0bd8
viniciuspra <vinicius.cascaesp@gmail.com>
2024-03-15 17:28:56 -0300

    feat: Add contact page to routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
+
 import Root from "@/pages/root";
 import MainContent from "@/pages/main";
 import AboutContent from "@/pages/about";
 import ErrorContent from "@/pages/404";
 import ProjectsContent from "@/pages/projects";
 import ProjectInfo from "@/pages/project-info";
+import ContactContent from "@/pages/contact";
 
 export const router = createBrowserRouter([
   {
@@ -27,6 +29,10 @@ export const router = createBrowserRouter([
       {
         path: "/projetos/:projectId",
         element: <ProjectInfo />,
+      },
+      {
+        path: "/contato",
+        element: <ContactContent />,
       },
     ],
   },

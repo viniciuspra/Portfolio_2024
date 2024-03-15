d76c617e4c538aaecac0d6a420bcd7ade7c765be
viniciuspra <vinicius.cascaesp@gmail.com>
2024-03-15 15:38:16 -0300

    feat: Add route provider to main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+
 import { ThemeProvider } from "./components/theme-provider.tsx";
+import { RouterProvider } from "react-router-dom";
+import { router } from "@/routes/index.tsx";
+
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="default" storageKey="portfolio-vinicius-theme">
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </React.StrictMode>
 );

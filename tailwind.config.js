e339b8c0799863c77b299cf4385b8fc81dfafd5c
viniciuspra <vinicius.cascaesp@gmail.com>
2024-03-15 15:40:27 -0300

    feat: Add customized shadow and animation in Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,10 @@ export default {
           stroke: "var(--card-stroke)",
         },
         muted: "var(--muted)",
-        primary: "var(--primary)",
+        primary: {
+          DEFAULT: "var(--primary)",
+          opacity: "var(--primary-opacity)",
+        },
         button: {
           DEFAULT: "var(--button)",
           stroke: "var(--button-stroke)",
@@ -22,6 +25,20 @@ export default {
       },
       borderWidth: {
         1.5: "1.5px",
+      },
+      boxShadow: {
+        "cv-button": "0 3px 40px -10px var(--button)",
+        "h-button": "0 3px 45px -6px var(--button)",
+        "git-button": "0 3px 50px -14px var(--muted)",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { transform: "translateY(-100px)", opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn 0.3s ease-out",
       },
     },
   },

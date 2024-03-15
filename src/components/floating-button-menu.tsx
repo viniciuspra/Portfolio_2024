497a82ee79ccb4a32861e6ec5ee9ffe8c72adcae
viniciuspra <vinicius.cascaesp@gmail.com>
2024-03-15 16:12:50 -0300

    feat: Modify position of floating button

--- src/components/floating-button-menu.tsx
+++ src/components/floating-button-menu.tsx
@@ -14,15 +14,17 @@ export default function FloatingButtonMenu() {
   };
 
   return (
-    <div className="fixed top-1/2 right-10 transform translate-y-2/4 translate-x-2/4">
+    <div className="fixed top-1/2 left-1 transform translate-y-2/4 translate-x-2/4">
       <button
-        className="rounded-full w-10 h-10 cursor-pointer transition-all hover:brightness-110 hover:animate-pulse flex justify-center items-center"
+        className={`rounded-full w-10 h-10 cursor-pointer transition-all hover:brightness-110 hover:animate-none flex justify-center items-center ${
+          isMenuOpen ? "" : "animate-pulse"
+        }`}
         onClick={toggleMenu}
       >
         <Icon />
       </button>
       {isMenuOpen && (
-        <div className="absolute top-1/2 transform -translate-y-1/2 right-11 bg-background border-1.5 border-card-stroke z-50 w-60 rounded-md h-fit">
+        <div className="absolute top-1/2 transform -translate-y-1/2 left-11 bg-background border-1.5 border-card-stroke z-50 w-60 rounded-md h-fit">
           <div className="h-12 bg-card-stroke rounded-t-md absolute top-0 left-0 w-full -z-40" />
           <div className="space-y-6">
             <div className="mt-6 flex items-center justify-between px-3">

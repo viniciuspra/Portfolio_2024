59726b04d7c484e2a77b535befbec45d52e19567
viniciuspra <vinicius.cascaesp@gmail.com>
2024-03-15 15:43:08 -0300

    feat: Update header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,13 @@
-import icon from "@/assets/icon.svg";
+import { Link } from "react-router-dom";
+import Icon from "./icon";
 
 export default function Header() {
   return (
-    <header className="flex items-center ">
-      <img src={icon} alt="icon" className="w-10 h-10" />
-      <div className="text-muted font-semibold flex-1 flex items-center justify-end h-10 border-1.5 border-card-stroke rounded-full px-10 bg-card">
+    <header className="flex items-center group">
+      <Link to="/">
+        <Icon />
+      </Link>
+      <div className="font-semibold uppercase flex-1 flex items-center justify-end h-10 border-1.5 border-card-stroke rounded-full px-10 bg-card hover:border-primary transition-colors">
         <p className="text-lg">Vinicius Prá</p>
       </div>
     </header>

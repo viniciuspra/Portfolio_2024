942f7c2ea15b7d6d5ca01dbaf7cb8f913dbe3dc1
viniciuspra <vinicius.cascaesp@gmail.com>
2024-03-15 15:34:50 -0300

    feat: Create application routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,33 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "@/pages/root";
+import MainContent from "@/pages/main";
+import AboutContent from "@/pages/about";
+import ErrorContent from "@/pages/404";
+import ProjectsContent from "@/pages/projects";
+import ProjectInfo from "@/pages/project-info";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorContent />,
+    children: [
+      {
+        path: "/",
+        element: <MainContent />,
+      },
+      {
+        path: "/sobre",
+        element: <AboutContent />,
+      },
+      {
+        path: "/projetos",
+        element: <ProjectsContent />,
+      },
+      {
+        path: "/projetos/:projectId",
+        element: <ProjectInfo />,
+      },
+    ],
+  },
+]);
